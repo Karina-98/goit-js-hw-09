941a19dee5e9759d0836424f0d23eb1286b116c9
Karina-98 <115569415+Karina-98@users.noreply.github.com>
2023-04-12 11:14:36 +0000

    Deploying to gh-pages from @ Karina-98/goit-js-hw-09@153a12d13fbafd11638f2b216e0cbd5e62116d0b 🚀

---
+++ 01-color-switcher.7bd3908f.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("type=button [data-start]"),e=document.querySelector("button [data-stop]");t.addEventListener("click",(function(){console.log("hello world")})),e.addEventListener("click",onClickStop)}();
+//# sourceMappingURL=01-color-switcher.7bd3908f.js.map
