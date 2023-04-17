07213dcb851ba488d4b5b79474dd5fc61ed382fc
Karina-98 <115569415+Karina-98@users.noreply.github.com>
2023-04-17 21:37:44 +0200

    add

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,11 +3,19 @@ const flatpickr = require("flatpickr");
 // es modules are recommended, if available, especially for typescript
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
 
-const inputText = document.getElementById('datetime - picker');
-const btn = document.getElementsByName('button')
 
-// flatpickr(inputText, options)
+const inputText = document.querySelector('input#datetime-picker');
+const btn = document.querySelector('button[data-start]');
+const day = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
+
+
+
+btn.disabled = true;
 
 const options = {
   enableTime: true,
@@ -15,12 +23,58 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
+    if (selectedDates[0].getDate() < options.defaultDate) {
+      btn.removeAttribute('disabled');
+    } else {
+      btn.disabled = true;
+       Notiflix.Notify.failure("Please choose a date in the future");
+    }
+ 
+  }
 };
-const data = new Date();
-console.log(defaultDate);
 
-// onclose(selectedDates) {
-// if (selectedDates )
-// }
+const fl = flatpickr(inputText, options);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+const data = new Date().getDate();
+
+
+
+btn.addEventListener('click', () => {
+  btn.disabled = true;
+  let interval = setInterval(() => { 
+    let currentTime = new Date().getTime();
+    let choosedate = fl.selectedDates[0] - currentTime;
+    let timer = convertMs(choosedate);
+
+    day.textContent = addLeadingZero(timer.days);
+    hours.textContent = addLeadingZero(timer.hours);
+    minutes.textContent = addLeadingZero(timer.minutes);
+    seconds.textContent = addLeadingZero(timer.seconds);
+
+  })
+
+  // if (msToCount < 1000) {
+  //     clearInterval(interval);
+  //   }
+  }, 1000 );
+
