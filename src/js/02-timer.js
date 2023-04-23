ab6085c587b92a27cf3fb2fdf05f34e21e177f46
Karina-98 <115569415+Karina-98@users.noreply.github.com>
2023-04-23 22:19:27 +0200

    +

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,4 +1,4 @@
-const flatpickr = require("flatpickr");
+// const flatpickr = require("flatpickr");
 
 // es modules are recommended, if available, especially for typescript
 import flatpickr from "flatpickr";
